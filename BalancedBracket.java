101c60e46296f6293c589c099266040edfa52979
Narasimha40 <114424561+Narasimha40@users.noreply.github.com>
2022-11-08 22:10:22 +0530

    Add files via upload

---
+++ BalancedBracket.java
@@ -0,0 +1,68 @@
+package com.glearning.lab3;
+ 
+ import java.util.Stack;
+import java.util.Scanner;
+
+public class BalancedBracket {
+	
+  private static boolean BalancedBrackets(String str) {
+	   Stack<Character> stack = new Stack<Character>();
+	   
+
+
+	   char[] charArray =str.toCharArray();
+	  
+	   for (int i=0;i<charArray.length;i++){
+		   char x = charArray[i];
+		   
+		   if(x =='(' || x =='{' || x =='[') {
+			   stack.push(x);
+			   continue;
+		    }
+		   
+		   if(stack.isEmpty()) {
+			   return false;
+		   }
+		   char check;
+		   switch(x) {
+		   case '}':
+			   check=(char)stack.pop();
+			   if(check=='('||check =='[')
+				   return false;
+			   break;
+		    
+		   case ']':
+			   check= (char)stack.pop();
+			   if(check=='('||check =='{')
+				   return false;
+			   break;
+		   
+		   case ')':
+			   check= (char)stack.pop();
+			   if(check=='{'||check =='[')
+				   return false;
+			   break;
+		   }
+			   
+		   }
+	   
+	   return (stack.isEmpty());
+	   
+  }
+   public static void main(String[] args) {
+	   
+	    String inputString;
+	    Scanner sc = new Scanner(System.in);
+	    System .out.println("enter the expression to check for balancing brackets");
+	    inputString=sc.nextLine();
+	    
+	    
+	 if(BalancedBrackets(inputString))
+		 System .out.println("Input String" +inputString+"is balanced");
+	 else {
+		 System .out.println("Input String" +inputString+"is not balanced");
+	 }
+		 sc.close();
+		 
+ }
+}
